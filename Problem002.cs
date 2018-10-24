12e9f314a6bc7930cc4aaa193dc852d1555d2770
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-10-23 19:56:50 -0700

    Add files via upload

---
+++ Problem002.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem002
+    {
+        public static string Fibonacci()
+        {
+            int sum = 0;
+            int max = 4000000;
+            int a = 1, b = 2;
+            int c;
+
+            while (sum <= max) //loop until 4mil
+            {
+                //only work with the even terms
+                if (a % 2 == 0)
+                    //add term 1 to the total
+                    sum += a;
+                //a variable to hold current 2 numbers to be added
+                c = a + b;
+                //move on to the next 2 numbers
+                a = b;
+                b = c;
+            }
+            
+            string s = "Fibonacci: " + sum.ToString();
+            return s;
+
+        }
+    }
+}
