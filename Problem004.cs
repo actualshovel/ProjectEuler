0eb1139d124a209c22b604f9a4ad018a2109892e
domFromCA <40729236+domFromCA@users.noreply.github.com>
2019-02-10 17:18:32 -0800

    Add files via upload

---
+++ Problem004.cs
@@ -0,0 +1,50 @@
+﻿/* A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+* Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+using System;
+class Problem004
+{
+static void Main(string[] args)
+    {
+        int product = 0;
+        int max = 0;
+        
+        for(int i = 999; i > 100; i--)
+        {
+            for(int j = 999; j > 100; j--)
+            {
+                product = i*j;
+                if (IsPalindrome(product))
+                {
+                    if (product > max)
+                        max = product;
+                }
+            }
+
+        }
+        Console.WriteLine("Highest palindrom made from the product of 2 3 digit numbers is: " + max);
+        
+        Console.ReadLine();
+    }
+
+    //get the remainder, divide number by 10, multiply by 10 & add remainder, repeat until 0
+    //number will be flipped. compare with parameter for palindrome
+    private static bool IsPalindrome(int x)
+    {
+        int temp = x;
+        int reverse = 0;
+        int remainder = 0;
+
+        while (temp > 0)
+        {
+            remainder = temp % 10;
+            temp /= 10;
+            reverse = reverse * 10 + remainder;
+        }
+        if (x == reverse)
+            return true;
+        else
+            return false;
+    }
+}
